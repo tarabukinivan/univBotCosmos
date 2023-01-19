8de9bb412afeb5ec8bbe734963e154c353f1f185
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2023-01-19 21:38:28 +0900

    Update func.js

--- requests/func.js
+++ requests/func.js
@@ -6,5 +6,10 @@ module.exports = function(commd, txdata =''){
         console.log(e);
         tmp = 'Error get logs';
     }
+    //console.log("kolvo slov:" +tmp.length)
+    if(tmp.length>3900){
+        //ограничение 4096
+        tmp=tmp.slice(0, 3900)
+    }
     return tmp
 }
