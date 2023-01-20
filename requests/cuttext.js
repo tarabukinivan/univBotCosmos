b80b10884dde06e3d9d38d11a0c90723c9171d6a
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2023-01-20 09:59:47 +0900

    Update cuttext.js

--- requests/cuttext.js
+++ requests/cuttext.js
@@ -1,7 +1,12 @@
-module.exports = function(tmp=''){
+module.exports = function(tmp='',r=''){
     if(tmp.length>3900){
         //ограничение 4096
-        tmp=tmp.slice(0, 3900)
+        if(r){
+            tmp=tmp.slice(tmp.length-3900, tmp.length-1)
+        }else{
+            tmp=tmp.slice(0, 3900)
+        }
+        
     }
     return tmp
 }
