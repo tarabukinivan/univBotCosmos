ceab7d41580ca18d194a8e25d3df61c7c4d93cee
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2023-01-20 13:47:09 +0900

    Update func.js

--- requests/func.js
+++ requests/func.js
@@ -1,7 +1,7 @@
 const shell = require('shelljs');
 module.exports = function(commd, txdata =''){
     try {
-        tmp = shell.exec(commd, {silent: true, shell: '/bin/bash'}).stdout;
+        tmp = shell.exec(commd, {silent: true, shell: '/bin/bash'}).stdout.trim();
     } catch (e) {
         console.log(e);
         tmp = 'Error get logs';
