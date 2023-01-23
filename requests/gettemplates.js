9197a13d22af958e39d76df3f02e6cc0730cfd3c
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2023-01-23 22:55:06 +0900

    Update gettemplates.js

--- requests/gettemplates.js
+++ requests/gettemplates.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-module.exports = function(rpc='',httprpc='',chainid='',valoper='',wallet=''){
+module.exports = function(rpc='',httprpc='',chainid='',valoper='',wallet='<wallet>',denom='<denom>'){
   let fileContent =''
   try{
     fileContent = fs.readFileSync("templates.txt", "utf8");    
@@ -13,6 +13,7 @@ module.exports = function(rpc='',httprpc='',chainid='',valoper='',wallet=''){
   totalcontent+="{httprpc}=" +httprpc + "\n\n"
   totalcontent+="{chainid}=" +chainid + "\n\n"
   totalcontent+="{valoper}=" +valoper + "\n\n"
+  totalcontent+="{denom}=" +denom + "\n\n"
   totalcontent+="{wallet}=" +wallet + "\n\n\n"
   let replacing=fileContent.replace(/{rpc}/gi, rpc)
   replacing=fileContent.replace(/{httprpc}/gi, httprpc)
